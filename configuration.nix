8fecd5e2b935a9fb563e28ac5262e0163f7d03c7
ibuysausage <bannansmooth68@proton.me>
2026-07-31 18:31:42 -0400

    change bootloader to grub

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,14 @@
       ./hardware-configuration.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+    };
+    efi.canTouchEfiVariables = true;
+  };
 
   networking.hostName = "nixos";
 
