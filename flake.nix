5a5a0f67b8d537b56609fa6af85c85d5c3f2cbdb
ibuysausage <bannansmooth68@proton.me>
2026-07-31 19:22:14 -0400

    add flakes

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
