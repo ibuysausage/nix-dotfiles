c85b01b9486694d0c6e18fef6384cb05acf01aed
ibuysausage <bannansmooth68@proton.me>
2026-07-31 20:00:41 -0400

    add home manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager,... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+	    extraSpecialArgs = { inherit inputs; };
+            users.byte = ./home.nix;
+          };
+        }
       ];
     };
   };
