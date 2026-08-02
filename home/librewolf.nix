0603086c7de76311630ab99dc352bf0dc69c3503
ibuysausage <bannansmooth68@proton.me>
2026-08-01 20:38:03 -0400

    finish librewolf

--- home/librewolf.nix
+++ home/librewolf.nix
@@ -5,6 +5,19 @@
   programs.librewolf = {
     enable = true;
 
+    profiles.byte = {
+      id = 0;
+      isDefault = true;
+      path = "default";
+
+      settings = {
+        toolkit.legacyUserProfileCustomizations.stylesheets = true;
+        svg.context-properties.content.enabled = true;
+        browser.urlbar.scotchBonnet.enableOverride = true;
+        sidebar.revamp = false;
+      };
+    };
+
     globalExtensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
       darkreader
