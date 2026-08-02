e0ce1f33ca7c4ccc76d024ca0f61065e8591e7f8
ibuysausage <bannansmooth68@proton.me>
2026-08-01 20:42:41 -0400

    add homefile.nix

--- home/home.nix
+++ home/home.nix
@@ -3,6 +3,7 @@
 {
 
   imports = [
+    ./homefile.nix
     ./sway.nix
     ./swaylock.nix
     ./git.nix
@@ -16,41 +17,6 @@
     # inputs.nur.legacyPackages.x86_64-linux.repos.rycee.firefox-addons.ublock-origin
     # hello
   ];
-
-
-  home.file."Pictures/Wallpapers" = {
-    source = ./wallpapers;
-    recursive = true;
-  };
-
-  home.file.".config/sway/status.sh" = {
-    source = ./scripts/status.sh;
-  };
-
-  home.file.".config/rofi" = {
-    source = ./themes/rofi;
-  };
-
-  home.file.".config/ohmyposh/omp.toml" = {
-    source = ./omp/omp.toml;
-  };
-
-  home.file.".config/librewolf/librewolf/profiles.ini" = {
-    source = ./librewolf/profiles.ini;
-  };
-
-  home.file.".config/librewolf/librewolf/default/chrome/userChrome.css" = {
-    source = ./librewolf/userChrome.css;
-  };
-
-  home.file.".config/librewolf/librewolf/default/chrome/userContent.css" = {
-    source = ./librewolf/userContent.css;
-  };
-
-  home.file.".config/librewolf/librewolf/default/chrome/assets" = {
-    source = ./librewolf/assets;
-    recursive = true;
-  };
 
   home.username = "byte";
   home.homeDirectory = "/home/byte";
---
+++ home/homefile.nix
@@ -0,0 +1,39 @@
+{ config, ... }:
+
+{
+
+  home.file."Pictures/Wallpapers" = {
+    source = ./wallpapers;
+    recursive = true;
+  };
+
+  home.file.".config/sway/status.sh" = {
+    source = ./scripts/status.sh;
+  };
+
+  home.file.".config/rofi" = {
+    source = ./themes/rofi;
+  };
+
+  home.file.".config/ohmyposh/omp.toml" = {
+    source = ./omp/omp.toml;
+  };
+
+  home.file.".config/librewolf/librewolf/profiles.ini" = {
+    source = ./librewolf/profiles.ini;
+  };
+
+  home.file.".config/librewolf/librewolf/default/chrome/userChrome.css" = {
+    source = ./librewolf/userChrome.css;
+  };
+
+  home.file.".config/librewolf/librewolf/default/chrome/userContent.css" = {
+    source = ./librewolf/userContent.css;
+  };
+
+  home.file.".config/librewolf/librewolf/default/chrome/assets" = {
+    source = ./librewolf/assets;
+    recursive = true;
+  }; 
+
+}
