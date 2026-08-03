13ddc093f6bc7a03c364237f5a8b8dff0259cb69
ibuysausage <bannansmooth68@proton.me>
2026-08-03 12:46:14 -0400

    add command running in nvim

--- home/nvim/lua/config/keybinds.lua
+++ home/nvim/lua/config/keybinds.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", "<cmd>Telescope project_cli_commands open<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>;", "<cmd>Telescope project_cli_commands running<CR>", { noremap = true, silent = true })
