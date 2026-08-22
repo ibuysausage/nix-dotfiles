cfa136e4ff3fc5d116d958fb213c9da73e23798f
ibuysausage <bannansmooth68@proton.me>
2026-08-22 06:40:44 -0400

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
