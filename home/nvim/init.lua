210abb56bbee10027cb1c63a8f53955cdbb9b296
ibuysausage <bannansmooth68@proton.me>
2026-08-23 20:28:55 -0400

    clean up neovim config

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -4,23 +4,3 @@ require('config.autocmd')
 require('config.lazy')
 require('config.lsp')
 require('config.cmp')
-
-vim.lsp.handlers["textDocument/hover"] =
-	vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
-
-vim.lsp.handlers["textDocument/signatureHelp"] =
-	vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "nix",
-	callback = function()
-		vim.bo.tabstop = 2  -- number of spaces a tab counts for
-		vim.bo.shiftwidth = 2 -- spaces used for indentation
-		vim.bo.softtabstop = 2 -- spaces when pressing Tab
-		vim.bo.expandtab = true -- use spaces instead of tabs
-	end,
-})
--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -3,6 +3,13 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 
-
-
-
+-- .nix files get 2 tab indent
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nix",
+	callback = function()
+		vim.bo.tabstop = 2 -- number of spaces a tab counts for
+		vim.bo.shiftwidth = 2 -- spaces used for indentation
+		vim.bo.softtabstop = 2 -- spaces when pressing Tab
+		vim.bo.expandtab = true -- use spaces instead of tabs
+	end,
+})
