ff43ef5d29b2d0d0ca2895b956fb0645141be3c2
ibuysausage <bannansmooth68@proton.me>
2026-08-25 12:44:18 -0400

    try to fix @DEFAULT_AUDIO_SINK

--- home/single.nix
+++ home/single.nix
@@ -5,4 +5,10 @@
   dconf.enable = true;
   gtk.enable = true;
   qt.enable = true;
+
+  services.pipewire.wireplumber.extraConfig."51-default-sink" = {
+    "wireplumber.settings" = {
+      "device.routes.default-sink" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+    };
+  };
 }
