14fea76f5dd0f3d1dec2665ee519d783d5da94b1
ibuysausage <bannansmooth68@proton.me>
2026-08-25 12:48:35 -0400

    fix wireplumber config

--- modules/programs.nix
+++ modules/programs.nix
@@ -4,9 +4,13 @@
   programs.neovim.enable = true;
   programs.firefox.enable = true;
 
-  services.pipewire.wireplumber.configs."51-default-sink" = {
-    "wireplumber.settings" = {
-      "device.routes.default-sink" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+  services.pipewire.wireplumber = {
+    enable = true;
+
+    extraConfig."51-default-sink" = {
+      "wireplumber.settings" = {
+        "device.routes.default-sink" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+      };
     };
   };
 }
