cf80e0313140dc118f9b53c06ce12d22887b3cc6
ibuysausage <bannansmooth68@proton.me>
2026-08-25 12:46:08 -0400

    not wpctl homemanager

--- home/single.nix
+++ home/single.nix
@@ -5,10 +5,4 @@
   dconf.enable = true;
   gtk.enable = true;
   qt.enable = true;
-
-  services.pipewire.wireplumber.extraConfig."51-default-sink" = {
-    "wireplumber.settings" = {
-      "device.routes.default-sink" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
-    };
-  };
 }
--- modules/programs.nix
+++ modules/programs.nix
@@ -3,4 +3,10 @@
   programs.zsh.enable = true;
   programs.neovim.enable = true;
   programs.firefox.enable = true;
+
+  services.pipewire.wireplumber.configs."51-default-sink" = {
+    "wireplumber.settings" = {
+      "device.routes.default-sink" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+    };
+  };
 }
