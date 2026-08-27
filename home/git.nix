3ff7277b6591a8b20c2a9471a124b6081129a17c
ibuysausage <bannansmooth68@proton.me>
2026-08-27 15:24:51 -0400

    anoying git junk

--- home/git.nix
+++ home/git.nix
@@ -6,7 +6,8 @@
       user.name = "ibuysausage";
       user.email = "bannansmooth68@proton.me";
 
-      pull.rebase = true;
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
     };
   };
 }
