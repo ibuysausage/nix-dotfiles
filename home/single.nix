865d46447b22ce6219b9d14b432ce75dfb191c3a
ibuysausage <bannansmooth68@proton.me>
2026-09-04 16:20:39 -0400

    add yazi

--- home/single.nix
+++ home/single.nix
@@ -2,6 +2,7 @@
   programs.btop.enable = true;
   programs.fzf.enable = true;
   programs.lazygit.enable = true;
+  programs.yazi.enable = true;
   dconf.enable = true;
   gtk.enable = true;
   qt.enable = true;
