f8e610ce8d6cfed7b6223aef23fc712ecbebade3
ibuysausage <bannansmooth68@proton.me>
2026-09-05 09:02:19 -0400

    add neovim-nightly cachix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
       "https://niri-epireyn.cachix.org"
       "https://grub2-themes.cachix.org"
       "https://ibuysausage-nur.cachix.org"
+      "https://ibuysausage-neovim-nightly.cachix.org"
       "https://nix-community.cachix.org"
       "https://fenix.cachix.org"
     ];
@@ -14,6 +15,7 @@
       "niri-epireyn.cachix.org-1:tlVyFN7CtsDT+ZcLPS+ekFWeT1X6X4OqvWqbBMyIzFA="
       "grub2-themes.cachix.org-1:lmVtdlFNnVzVqgikQDgstzV0tdzA64pDxwEbyykfW14="
       "ibuysausage-nur.cachix.org-1:tqoAgFo/8AL/GhbOg9Cp9Fc46hHBnOCOw5V46knhH9I="
+      "ibuysausage-neovim-nightly.cachix.org-1:CcVje9KrKo6+tyVied3wfzmAmpyNPysard1GGaNoIoY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "fenix.cachix.org-1:ecJhr+RdYEdcVgUkjruiYhjbBloIEGov7bos90cZi0Q="
     ];
