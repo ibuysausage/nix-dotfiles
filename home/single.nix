8c6524d26a4f74bd04bc9e0d31e840cfcaaa8136
ibuysausage <bannansmooth68@proton.me>
2026-09-06 09:16:56 -0400

    set default browser to librewolf

--- home/single.nix
+++ home/single.nix
@@ -6,4 +6,15 @@
   dconf.enable = true;
   gtk.enable = true;
   qt.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+    };
+  };
 }
