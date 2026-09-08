cc377ca183cba4895fe339ad25f48b1a6f3987d8
ibuysausage <bannansmooth68@proton.me>
2026-09-08 06:14:05 -0400

    add accel profile and speed

--- modules/wildfire/programs.nix
+++ modules/wildfire/programs.nix
@@ -16,7 +16,13 @@
   services.xserver.videoDrivers = ["modesetting"];
   # tpad
   services.libinput.enable = true;
-  services.libinput.touchpad.tapping = true;
+  services.libinput = {
+    touchpad = {
+      tapping = true;
+      accelProfile = "flat";
+      accelSpeed = "0.9";
+    };
+  };
   # caps to esc for neovim
   services.xserver.xkb.options = "caps:escape";
 
