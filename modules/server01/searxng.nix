dfed7232c8bdc8525ddd42ac059bc9fb2257c3b3
ibuysausage <bannansmooth68@proton.me>
2026-09-13 18:48:08 -0400

    im prob stupid

--- modules/server01/searxng.nix
+++ modules/server01/searxng.nix
@@ -1,9 +1,13 @@
-{...}: {
+{pkgs, ...}: {
+  enviroment.systemPackages = with pkgs; [
+    searxng
+  ];
+
   services.searx = {
     enable = true;
     redisCreateLocally = true;
     settings.server = {
-      bind_address = "192.168.4.30";
+      bind_address = "0.0.0.0";
       port = 8080;
       # WARNING: setting secret_key here might expose it to the nix cache
       # see below for the sops or environment file instructions to prevent this
