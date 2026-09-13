632083a8f6e7b82beea55dcc8bfdc83b345720b6
ibuysausage <bannansmooth68@proton.me>
2026-09-13 16:14:09 -0400

    change to private ip

--- modules/server01/searxng.nix
+++ modules/server01/searxng.nix
@@ -3,7 +3,7 @@
     enable = true;
     redisCreateLocally = true;
     settings.server = {
-      bind_address = "127.0.0.1";
+      bind_address = "192.168.4.4.30";
       port = 8080;
       # WARNING: setting secret_key here might expose it to the nix cache
       # see below for the sops or environment file instructions to prevent this
