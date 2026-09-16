26fd8435177edc78ce312ea9889da3db977391a9
ibuysausage <bannansmooth68@proton.me>
2026-09-16 19:51:01 -0400

    sway pointer speed to 0.6

--- home/sway.nix
+++ home/sway.nix
@@ -95,7 +95,7 @@
 
         "type:pointer" = {
           accel_profile = "flat";
-          pointer_accel = "0.7";
+          pointer_accel = "0.6";
           natural_scroll = "enabled";
         };
       };
