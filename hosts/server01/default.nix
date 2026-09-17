16a68b268f7009a5c8ea61cbaa1f262982912dcc
ibuysausage <bannansmooth68@proton.me>
2026-09-17 18:20:16 -0400

    re add adguardhome for server01

--- hosts/server01/default.nix
+++ hosts/server01/default.nix
@@ -8,7 +8,7 @@
     ../../modules/server01/packages.nix
     ../../modules/server01/system.nix
     ../../modules/server01/searxng.nix
-    # ../../modules/server01/adguardhome.nix
+    ../../modules/server01/adguardhome.nix
     ../../modules/server01/stylix.nix
   ];
 
