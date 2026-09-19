314c24b7f139f67e6e2667ee7e32f0f48a6d2576
ibuysausage <bannansmooth68@proton.me>
2026-09-19 09:09:43 -0400

    set ip4 manually server01

--- modules/server01/system.nix
+++ modules/server01/system.nix
@@ -25,6 +25,15 @@
     HandleLidSwitchDocked = "ignore";
   };
 
+  networking.interfaces.wlp1s0.ipv4.addresses = [
+    {
+      address = "192.168.4.30";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.4.1";
+  networking.nameservers = ["127.0.0.1"];
   networking.firewall.allowedTCPPorts = [8080 3000 8888 53];
   networking.firewall.allowedUDPPorts = [53];
 }
