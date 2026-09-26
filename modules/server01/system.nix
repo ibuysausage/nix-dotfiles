67ddfa1a7b4b8d4f048a06ef654597ad409840d1
ibuysausage <bannansmooth68@proton.me>
2026-09-25 20:28:32 -0400

    add systemd service for splatoon

--- modules/server01/system.nix
+++ modules/server01/system.nix
@@ -19,4 +19,23 @@ _: {
     HandleLidSwitchExternalPower = "ignore";
     HandleLidSwitchDocked = "ignore";
   };
+
+  systemd.services.splatnet3-token-util = {
+    description = "SplatNet3 token util";
+    wantedBy = ["multi-user.target"];
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "/root/splatoon/splatoon/bin/python /root/splatoon/splatnet3-token-util/run_s3s.py -r -M";
+      WorkingDirectory = "/root/splatnet3-token-util";
+      Restart = "always";
+      RestartSec = 5;
+
+      # Run as your normal user instead of root, since this presumably
+      # touches your own config/cookies
+      User = "root";
+    };
+  };
 }
